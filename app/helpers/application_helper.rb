ae5911dfc46f3f9574b35a4fba6c1aecaff0e009
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-01 11:00:32 -0400

    logout and home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def logged_in?(user)
     session.key?(:user_id) && session[:user_id] == user.id
   end
+
+  def current_user
+    User.find(session[:user_id])
+  end
 end
